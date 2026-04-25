095b70b041d95e061cf8b2d33b68670bf039bd15
earnwithalee <earnwithalee@gmail.com>
2026-04-25 16:23:22 +0500

    refactor: consolidate utility functions

--- src/App.jsx
+++ src/App.jsx
@@ -400,4 +400,5 @@ export default App;
  / /   R e f a c t o r   u p d a t e   3 4  
  / /   R e f a c t o r   u p d a t e   3 5  
  / /   R e f a c t o r   u p d a t e   3 6  
+ / /   R e f a c t o r   u p d a t e   3 7  
  
