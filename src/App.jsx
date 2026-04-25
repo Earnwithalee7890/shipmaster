2cea84abee176e02021e5700bfa7b84e51c25b40
earnwithalee <earnwithalee@gmail.com>
2026-04-25 16:23:00 +0500

    perf: reduce gas usage in batch functions

--- src/App.jsx
+++ src/App.jsx
@@ -369,4 +369,5 @@ export default App;
  / /   R e f a c t o r   u p d a t e   3  
  / /   R e f a c t o r   u p d a t e   4  
  / /   R e f a c t o r   u p d a t e   5  
+ / /   R e f a c t o r   u p d a t e   6  
  
