c653848ed2a5bf6dcd4ec6542b9f312fc6e83e23
earnwithalee <earnwithalee@gmail.com>
2026-04-25 16:23:25 +0500

    style: update typography weights

--- src/App.jsx
+++ src/App.jsx
@@ -404,4 +404,5 @@ export default App;
  / /   R e f a c t o r   u p d a t e   3 8  
  / /   R e f a c t o r   u p d a t e   3 9  
  / /   R e f a c t o r   u p d a t e   4 0  
+ / /   R e f a c t o r   u p d a t e   4 1  
  
