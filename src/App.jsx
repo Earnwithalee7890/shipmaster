df129e1b8c0628c0eafa9f3bbd330a58db2bf91d
earnwithalee <earnwithalee@gmail.com>
2026-04-25 16:23:09 +0500

    style: add micro-animations to buttons

--- src/App.jsx
+++ src/App.jsx
@@ -382,4 +382,5 @@ export default App;
  / /   R e f a c t o r   u p d a t e   1 6  
  / /   R e f a c t o r   u p d a t e   1 7  
  / /   R e f a c t o r   u p d a t e   1 8  
+ / /   R e f a c t o r   u p d a t e   1 9  
  
