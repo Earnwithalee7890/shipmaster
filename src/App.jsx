30a1ac7fbc5da886684af735f82ee749b2c06dae
earnwithalee <earnwithalee@gmail.com>
2026-04-25 16:23:10 +0500

    docs: update license information

--- src/App.jsx
+++ src/App.jsx
@@ -383,4 +383,5 @@ export default App;
  / /   R e f a c t o r   u p d a t e   1 7  
  / /   R e f a c t o r   u p d a t e   1 8  
  / /   R e f a c t o r   u p d a t e   1 9  
+ / /   R e f a c t o r   u p d a t e   2 0  
  
